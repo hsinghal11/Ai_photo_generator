8c37160a77c92baa3451725f8232c42bf1e9da9e
Himanshu Singhal <himanshusinghal11082004@gmail.com>
2025-05-09 02:49:12 +0530

    basic Prisma Schema Done

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,8 +1,38 @@
 import express from "express";
+import { prismaClient } from "db";
+import { GenerateImage, GenerateImagesFromPack, TrainModel } from "common/types";
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(express.json());
+
+app.post("/ai/training", async (req,res) =>{
+    try {
+        const parsedBody = TrainModel.safeParse(req.body);
+        if(!parsedBody.success){
+            res.status(411).json({
+                message: "Invalid request body",
+                error : parsedBody.error,
+            })
+            return;
+        }
+
+        await prismaClient.model.create({
+            data:{
+                name: parsedBody.data.name,
+                age: parsedBody.data.age,
+                bald: parsedBody.data.bald,
+                type: parsedBody.data.type,
+                eyeColor: parsedBody.data.eyeColor,
+                ethnicity: parsedBody.data.ethinicity,
+            }
+        })
+    } catch (err) {
+        
+    }
+})
+
 
 app.get("/", (req,res)=>{
     res.send("Hello from server");
